25ce6fa65e42ae28737caeb6b7e7320c1fc04fd0
Emmanuel Olannye <eeolannye@gmail.com>
2026-04-24 09:30:01 -0500

    Uncertain changes made here

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,4 +1,51 @@
-'use strict'
+// 'use strict'
+
+// const shapesArray = [];
+
+// class Shape {
+//     constructor(name, color) {
+//         this._name = name;
+//         this._color = color;
+//     }
+
+//     get name(){return this._name}
+//     get color(){return this._color}
+
+//     getInfo() {
+//         return `${this._name} with color ${this._color}`
+//     }
+// }
+
+// function createShape () {
+//     const shapeSelector = document.getElementById('display-grid-box').value;
+//     const colorSelector = document.getElementById("display-grid-tank").value;
+//     const gridBox = document.getElementById("some-container");
+
+//     if (shapesArray.length >= 20) {
+//         alert("You've reached the maximum number of shapes (20).")
+//         return
+//     }
+//     const newShape = new Shape(shapeSelector, colorSelector);
+//     shapesArray.push(newShape);
+
+//     const shapeBlock = document.createElement("div");
+//     shapeBlock.classList.add(shapeSelector);
+//     shapeBlock.style.backgroundColor = colorSelector
+//     // getInfo().style.color = rgb(255, 0, 0);
+
+//     shapeBlock.addEventListener("hover", function() {
+//         let ice = '<p>'
+//         console.log(newShape.getInfo())
+//     })
+
+//     gridBox.appendChild(shapeBlock)
+// }
+
+// const button = document.getElementById('thisButton');
+// button.addEventListener('click', createShape)
+
+
+'use strict';
 
 const shapesArray = [];
 
@@ -8,36 +55,56 @@ class Shape {
         this._color = color;
     }
 
-    get name(){return this._name}
-    get color(){return this._color}
+    get name() {
+        return this._name;
+    }
+
+    get color() {
+        return this._color;
+    }
 
     getInfo() {
-        return `${this._name} with color ${this._color}`
+        return `${this._name} with color ${this._color}`;
     }
 }
 
-function createShape () {
+function createShape() {
     const shapeSelector = document.getElementById('display-grid-box').value;
-    const colorSelector = document.getElementById("display-grid-tank").value;
-    const gridBox = document.getElementById("some-container");
+    const colorSelector = document.getElementById('display-grid-tank').value;
+    const gridBox = document.getElementById('some-container');
 
     if (shapesArray.length >= 20) {
-        alert("You've reached the maximum number of shapes (20).")
-        return
+        alert("You've reached the maximum number of shapes (20).");
+        return;
     }
+
     const newShape = new Shape(shapeSelector, colorSelector);
     shapesArray.push(newShape);
 
-    const shapeBlock = document.createElement("div");
+    const shapeBlock = document.createElement('div');
     shapeBlock.classList.add(shapeSelector);
-    shapeBlock.style.backgroundColor = colorSelector
+    shapeBlock.style.backgroundColor = colorSelector;
 
-    shapeBlock.addEventListener("click", function() {
-        console.log(newShape.getInfo())
-    })
+    shapeBlock.addEventListener('click', function () {
+        alert(newShape.getInfo());
+    });
 
-    gridBox.appendChild(shapeBlock)
+    gridBox.appendChild(shapeBlock);
+}
+
+function resetShapes() {
+    const gridBox = document.getElementById('some-container');
+
+    // Clear the array
+    shapesArray.length = 0;
+
+    // Remove all shapes from the screen
+    gridBox.innerHTML = '';
 }
 
 const button = document.getElementById('thisButton');
-button.addEventListener('click', createShape)
+button.addEventListener('click', createShape);
+
+// Reset button
+const resetButton = document.getElementById('resetButton');
+resetButton.addEventListener('click', resetShapes);
